9e954866c8aaf32f3dc830a5ede395c7b087f16f
sheryluEPN <USRDEL@USR21.local>
2017-05-09 15:39:15 -0500

    commit de bucles

---
+++ Javascript/Loops/loop.js
@@ -0,0 +1,10 @@
+//bucles en javascript
+//while, for
+
+for(var i=0; i<10; i++){
+    console.log(i);
+}
+var i=0;
+while(i<20){
+    console.log(i++);
+}
---
+++ Javascript/Sentencias de control/SDC.js
@@ -0,0 +1,21 @@
+// sentencias de control
+// if, else, switch
+
+var conhambre={nombre: 'nombrecito'};
+if(conhambre){
+    console.log('comer');
+}else{
+    console.log('trabajar')
+}
+
+var numerito=false;
+switch(numerito) {
+    case 1:
+        console.log('estas lejos')
+        break;
+    case 9:
+        console.log('por fin! era 9')
+        break;
+    default:
+        console.log('intenta de nuevo')
+}
