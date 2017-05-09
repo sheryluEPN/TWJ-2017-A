6606fdc22023ddfc06a67e5b68625931a7521e5f
sheryluEPN <USRDEL@USR21.local>
2017-05-09 16:31:03 -0500

    commit es5

--- Javascript/app.js
+++ Javascript/app.js
@@ -3,3 +3,22 @@
  */
 
 console.log('alguna cosa');
+
+var objeto1={
+    nombre: 'unnombre',
+    apellido: 'unapellido',
+    fecha: new Date(),
+    imprimir: function (){
+        console.log(this.nombre + ' '+  this.apellido);
+},
+    anio: function(){
+        return this.fecha.getFullYear();
+    },
+    diferenciaedad: function (edad1) {
+        return Math.abs(this.fecha.getFullYear()-edad1);
+    }
+}
+
+objeto1.imprimir();
+console.log(objeto1.anio());
+console.log(objeto1.diferenciaedad(1995));
