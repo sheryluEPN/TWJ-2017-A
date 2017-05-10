ab9e364f8850f8d5b8ba9b8287f33c0bac6303d9
sheryluEPN <USRDEL@USR21.local>
2017-05-10 14:14:11 -0500

    commit foreach

---
+++ Javascript/Foreach/foreach.js
@@ -0,0 +1,24 @@
+// FOR EACH
+// OPERADORES
+let arregloNumeros = [1,2,3,4,5,6];
+
+let resultado = arregloNumeros.forEach((numero,indice)=>{
+        console.log(indice,numero);
+})
+
+console.log('Resultado:',resultado)
+//forEach es usado para ejecutar código
+//el nùmero de veces de un arreglo
+
+elementos.forEach(function(value, key) {
+    console.log("value",value)
+    console.log("key",key)
+});
+
+function myforeach(arreglo, funcion){
+    for(var i=0; i< arreglo.length; i++){
+        funcion(i, arreglo[i], arreglo);
+    }
+}
+myforeach(elementos, (indice, valor)=> {
+    console.log('este es un indice:'+indice+' este es el valor:'+valor)});
