91e0d7acd2ddaf324516c024f3cba10810fa4f7e
sheryluEPN <USRDEL@USR21.local>
2017-05-17 14:47:56 -0500

    commit angular

--- Angular/bWeb/src/app/app.component.ts
+++ Angular/bWeb/src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'twj-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  noroot = 'no soy root';
+usuario: UsuarioInt ={
+  nombre: "unnombre",
+  apellido: "unapellido"
+}
+constructor(){
+  console.log("Constructor");
+  this.holamundo();
+  console.log(this.saludar(this.usuario.nombre));
+}
+holamundo(){
+  console.log("Wolis");
+}
+saludar(nombre: string, apellido?: string):string{
+return `woli tu ${nombre} ${apellido}`
+}
+}
+
+interface UsuarioInt {
+  nombre: string,
+  apellido: string
 }
