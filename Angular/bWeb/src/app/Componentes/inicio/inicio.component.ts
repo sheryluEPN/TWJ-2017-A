19dddc7b2f863079e38a29bce41b584db2c861e4
sheryluEPN <USRDEL@USR21.local>
2017-05-30 16:58:10 -0500

    commits bootstrap angular

--- Angular/bWeb/src/app/Componentes/inicio/inicio.component.ts
+++ Angular/bWeb/src/app/Componentes/inicio/inicio.component.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  nombre: string = "nombresito";
+  arregloUsr=[{
+    nombre:"nombrecito1",
+    apellido:"apellidito1"
+  },
+    {
+      nombre:"nombrecito2",
+      apellido:"apellidito2"
+    },
+    {
+      nombre:"nombrecito3",
+      apellido:"apellidito3"
+    }];
+  cambiarNombre(){
+    this.nombre="otro nombresito";
+  }
+
+  otroNombre():void{
+    this.nombre="un nombre de foco";
+  }
+
+  cambiarInput(nombreEtiqueta){
+  console.log(nombreEtiqueta.type);
+  console.log(nombreEtiqueta.value);
+    this.nombre= nombreEtiqueta.value;
+}
   constructor() { }
 
   ngOnInit() {
